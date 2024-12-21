377031b0f3ea86f055f58dbdf6b1c59733a1f4f2
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2024-12-21 13:22:50 +0800

    create README.md

--- k.c
+++ k.c
@@ -1,31 +1,18 @@
-#include <math.h>
 #include <stdio.h>
 
-#define N	200000
+#define N	500
 
 int main() {
-	int t, n;
-	long long k;
-	int i, j;
-	static int aa[N];
+	freopen("in", "r", stdin);
+	int t;
 
-	for (scanf("%d", &t); t--;) {
-		scanf("%d%lld", &n, &k);
-		if ((double) (n - 1) < log2(k))
-			puts("-1");
-		else {
-			int l = 0, r = n;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		int i, j;
+		int yes = 1;
+		static char cc[N + 1];
 
-			for (i = 0, j = n - 2; j >= 0; j--) {
-				if (log2(k) > (double) j)
-					k -= 1LL << j, aa[--r] = ++i;
-				else
-					aa[l++] = ++i;
-			}
-			aa[l++] = n;
-			for (i = 0; i < n; ++i)
-				printf("%d ", aa[i]);
-			puts("");
-		}
+		scanf("%d%s", &n, cc);
 	}
 }
