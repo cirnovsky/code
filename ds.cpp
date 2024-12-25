56f42fa69ad278417c4c53e3e8578319ab4ed3eb
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2024-12-25 23:26:01 +0800

    update ds.cpp; safer and neater

--- ds.cpp
+++ ds.cpp
@@ -1,6 +1,10 @@
 /// {{{
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <chrono>
 
 using ll = long long;
 using ull = unsigned long long;
@@ -9,45 +13,57 @@ using ldb = long double;
 
 #define pb push_back
 #define eb emplace_back
-#define mkp make_pair
+#define mkp std::make_pair
 #define allu(u) (u).begin(), (u).end()
-mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-#define rng(l, r) uniform_int_distribution<ll>(l, r)(rnd)
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+#define rng(l, r) std::uniform_int_distribution<ll>(l, r - 1)(rnd)
 
 #define rep1(n) for (int _ = 0; _ < n; ++_)
 #define rep2(i, n) for (int i = 0; i < (n); ++i)
 #define rep3(i, l, h) for (auto i = (l); i < (h); ++i)
 #define rep4(i, l, h, d) for (auto i = (l); i < (h); i += (d))
-#define drep1(n) for (int _ = (n)-1; _ >= 0; --_)
-#define drep2(i, n) for (int i = (n)-1; i >= 0; --i)
-#define drep3(i, h, l) for (auto i = (h)-1; i >= l; --i)
-#define drep4(i, h, l, d) for (auto i = (h)-1; i >= l; i -= (d))
+#define drep1(n) for (int _ = (n) - 1; _ >= 0; --_)
+#define drep2(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define drep3(i, h, l) for (auto i = (h) - 1; i >= l; --i)
+#define drep4(i, h, l, d) for (auto i = (h) - 1; i >= l; i -= (d))
 #define __overload(a, b, c, d, e, ...) e
 #define rep(...) __overload(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
 #define drep(...) __overload(__VA_ARGS__, drep4, drep3, drep2, drep1)(__VA_ARGS__)
 
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-using bsi = basic_string<int>;
-template <typename Tp> using vt = vector<Tp>;
-template <typename Tp> using bst = basic_string<Tp>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vll = std::vector<ll>;
+using vvll = std::vector<vll>;
+using bsi = std::basic_string<int>;
+template <typename Tp>
+using vt = std::vector<Tp>;
+template <typename Tp>
+using bst = std::basic_string<Tp>;
 
-template <typename Tp> void rd(Tp& x) { cin >> x; }
-template <typename Tp, typename... Args> void rd(Tp& x, Args&... args) { rd(x), rd(args...); }
-template <typename Tp> void rds(Tp* v, int n) { for (int i=0;i<n;++i) rd(v[i]); }
-template <typename Tp> void rds(vector<Tp>& v) { for (Tp& x : v) rd(x); }
+template <typename Tp> void rd(Tp& x) { std::cin >> x; }
+template <typename Tp, typename... Args>
+void rd(Tp& x, Args&... args) {
+	rd(x), rd(args...);
+}
+template <typename Tp>
+void rds(Tp* v, int n) {
+	for (int i = 0; i < n; ++i)
+		rd(v[i]);
+}
+template <typename Tp>
+void rds(std::vector<Tp>& v) {
+	for (Tp& x : v)
+		rd(x);
+}
 template <typename Tp>
 Tp& cmax(Tp& x, const Tp& y) {
-    return x = max(x, y);
+	return x = std::max(x, y);
 }
 template <typename Tp>
 Tp& cmin(Tp& x, const Tp& y) {
-    return x = min(x, y);
+	return x = std::min(x, y);
 } // }}}
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    return 0;
+	std::cin.tie(nullptr)->sync_with_stdio(0);
+	return 0;
 }
--- m.c
+++ m.c
@@ -1,40 +1,5 @@
 #include <stdio.h>
 
-#define N	50
-
 int main() {
-	freopen("in", "r", stdin);
-	int t;
-
-	scanf("%d", &t);
-	while (t--) {
-		int n;
-		int i, j, k;
-		int one = 1, zero = 1;
-		static int aa[N];
-
-		scanf("%d", &n);
-		for (i = 0; i < n; ++i)
-			scanf("%d", aa + i);
-		for (i = 0; i < n; ++i)
-			if (aa[i])
-				zero = 0;
-		for (i = 0; i < n; ++i)
-			if (aa[i])
-				break;
-		for (j = n; j--;)
-			if (aa[j])
-				break;
-
-		for (k = i; k <= j; ++k)
-			if (!aa[k])
-				one = 0;
-		if (zero)
-			puts("0");
-		else if (one)
-			puts("1");
-		else
-			puts("2");
-	}
 	return 0;
 }
