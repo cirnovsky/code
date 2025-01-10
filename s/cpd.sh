215b7f63b6240e9bbf60a2835cd98e5d4684ff7f
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2025-01-10 10:18:24 +0800

    update scripts; more reusable

--- s/cpd.sh
+++ s/cpd.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 
-# Check if at least one filename is provided
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <file1.cpp> [file2.cpp ...]"
-    exit 1
-fi
+DIR="$(pwd)/param"
 
-# Read compilation parameters from parameter.txt
-if [ -f parameter.txt ]; then
-    compilation_params=$(<parameter.txt)
-else
-    echo "parameter.txt not found. Exiting."
-    exit 1
-fi
+content=$(cat $DIR)
 
-# Loop through each provided filename
-for file in "$@"; do
-    # Check if the file exists and is a regular file
-    if [ -f "$file" ]; then
-        # Compile the file using g++
-        g++-14 $compilation_params "$file" -g -o "${file%.*}"  # Remove extension for output filename
+echo "$content -g" > $DIR
 
-        # Check if compilation was successful
-        if [ $? -eq 0 ]; then
-            echo "Compiled $file successfully."
-        else
-            echo "Failed to compile $file."
-        fi
-    else
-        echo "$file is not a valid file."
-    fi
-done
+_cp "$@"
+
+echo "$content" > $DIR
