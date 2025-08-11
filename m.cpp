b20d698ff790de424c0f27a0779ac33931fe23e0
cirnovsky <cirnovskyv@gmail.com>
2025-08-11 23:04:26 +0800

    fix ccr bug

--- m.cpp
+++ m.cpp
@@ -1,51 +1,46 @@
-// X1(a) X2(b)
-// Y1(c) Y2(d)
-// X1*Y1 -> X2*Y2
-// X1*Y2 -> X2*Y1
+// dp[i][s] + t + 1 -> dp[son[i][(s + t) % deg[i]]][s + t + 1]
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int N = 2e5;
-int xx[N], yy[N], tmp[N], tmp2[N], rev[N], rev2[N];
+const int N = 5e3;
+vector<int> adj[N];
+int f0[N], f1[N];
 
 void solve()
 {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i)
+	int n, m;
+	scanf("%d%d", &n, &m);
+	for (int i = 0, u, v; i < m; ++i)
 	{
-		scanf("%d%d", &xx[i], &yy[i]);
+		scanf("%d%d", &u, &v);
+		u--;
+		v--;
+		adj[u].push_back(v);
 	}
-	iota(tmp, tmp + n, 0);
-	iota(tmp2, tmp2 + n, 0);
-	sort(tmp, tmp + n, [&](int i, int j)
-			{
-			return xx[i] < xx[j];
-			});
-	sort(tmp2, tmp2 + n, [&](int i, int j)
-			{
-			return yy[i] < yy[j];
-			});
-	for (int i = 0; i < n; ++i)
+	const int INF = 0x3f3f3f3f;
+	memset(f0, 0x3f, n * sizeof *f0);
+	f0[0] = 0;
+	int t;
+	for (t = 0; t <= n; ++t)
 	{
-		rev[tmp[i]] = i;
-		rev2[tmp2[i]] = i;
+		for (int i = 0; i < n; ++i)
+		{
+			f1[i] = min(f1[i], f0[i] + 1);
+			f1[adj[i][t % adj[i].size()]] = min(f1[adj[i][t % adj[i].size()]], f0[i]);
+		}
+		if (f1[n - 1] != INF)
+		{
+			break;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			swap(f0[i], f1[i]);
+			f1[i] = INF;
+		}
 	}
-	vector<int> cat[4];
-	for (int i = 0; i < n; ++i)
-	{
-		cat[(rev[i] < n / 2) << 1 | (rev2[i] < n / 2)].push_back(i);
-	}
-	for (int i = 0; i < (int) cat[2].size(); ++i)
-	{
-		printf("%d %d\n", cat[2][i] + 1, cat[1][i] + 1);
-	}
-	for (int i = 0; i < (int) cat[3].size(); ++i)
-	{
-		printf("%d %d\n", cat[3][i] + 1, cat[0][i] + 1);
-	}
+	cout << t << " " << f1[n - 1] << "\n";
 }
 
 int main()
